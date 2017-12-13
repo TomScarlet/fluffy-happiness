175f2fbc39c739650a50b339f6d3f5ef792418d7
Martin Middelmann <martin@middelmanns.net>
2017-12-13 19:16:50 +1100

    More fixes from the merge

--- src/com/fluffyhappiness/Main.java
+++ src/com/fluffyhappiness/Main.java
@@ -1,6 +1,10 @@
 package com.fluffyhappiness;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
@@ -8,6 +12,9 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 import com.jme3.system.JmeContext;
+
+import net.mmarss.grease.ecs.Entity;
+import net.mmarss.grease.ecs.EntityManager;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -17,12 +24,26 @@ import com.jme3.system.JmeContext;
  */
 public class Main extends SimpleApplication {
 	
+	private FluffyTerrain terrain;
+	
+	private EntityManager entityManager;
+	
+	private Entity townCenter;
+	
+	private ButtonHolder holder;
+	
+	public Main() {
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		Main app = new Main();
-		AppSettings settings = new AppSettings(true);
 		
+		// App settings
+		AppSettings settings = new AppSettings(true);
 		settings.setResolution(1366, 768);
+		settings.setResizable(true);
 		app.setSettings(settings);
 		
 		app.start(JmeContext.Type.Display);
@@ -31,14 +52,54 @@ public class Main extends SimpleApplication {
 	@Override
 	public void simpleInitApp() {
 		
-		Box b = new Box(1, 1, 1);
-		Geometry geom = new Geometry("Box", b);
+		flyCam.setMoveSpeed(20f);
 		
-		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat.setColor("Color", ColorRGBA.Blue);
-		geom.setMaterial(mat);
+		// Create terrain
+		terrain = new FluffyTerrain(assetManager);
+		terrain.initLod(getCamera());
+		terrain.attachTo(rootNode);
 		
-		rootNode.attachChild(geom);
+		// Game entities
+		entityManager = new EntityManager();
+		
+		// Town center
+		townCenter = entityManager.createEntity();
+		Geometry geometry = new Geometry("Box", new Box(1, 1, 1));
+		geometry.setLocalTranslation(0, terrain.getHeight(0, 0), 0);
+		Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		material.setColor("Color", ColorRGBA.Blue);
+		geometry.setMaterial(material);
+		entityManager.addComponent(townCenter, new RenderableComponent(geometry), RenderableComponent.class);
+		
+		entityManager.getComponent(townCenter, RenderableComponent.class).attachTo(rootNode);
+		
+		// Escape key toggles menu
+		inputManager.deleteMapping(SimpleApplication.INPUT_MAPPING_EXIT);
+		inputManager.addMapping("Menu", new KeyTrigger(KeyInput.KEY_ESCAPE));
+		inputManager.addListener(new ActionListener() {
+			
+			private boolean menu = false;
+			
+			@Override
+			public void onAction(String name, boolean isPressed, float tpf) {
+				
+				if (!isPressed) {
+					flyCam.setEnabled(menu);
+					inputManager.setCursorVisible(!menu);
+					menu = !menu;
+				}
+			}
+		}, "Menu");
+		
+		holder = new ButtonHolder(guiNode, assetManager);
+		inputManager.addMapping("MouseLeftClicked", new MouseButtonTrigger(mouseInput.BUTTON_LEFT));
+		
+		inputManager.addListener((ActionListener) (name, isPressed, tpf) -> {
+			if (name.equals("MouseLeftClicked") && isPressed == true) {
+				holder.CheckMousePos(inputManager.getCursorPosition());
+			}
+		}, "MouseLeftClicked");
+		
 	}
 	
 	@Override
